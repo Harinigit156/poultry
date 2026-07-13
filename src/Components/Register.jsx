c501b0e22a2cbc5d825db59eeb676c9cb5b9507a
Harini KV <harinikv35@gmail.com>
2026-07-13 19:56:40 +0530

    Updated backend API URL

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -16,7 +16,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "http://poultry-2-i927.onrender.com//api/auth/register",
         {
           name,
           email,
